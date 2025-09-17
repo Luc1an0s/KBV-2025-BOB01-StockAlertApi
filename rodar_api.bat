abeb0989190858f8b320311e5962f842d5ecc211
Luc1an0s <lucianomaciel53@gmail.com>
2025-09-17 17:22:00 -0400

    a

--- rodar_api.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Ativa o ambiente virtual
-call "C:\Users\Lucius\Documents\GitHub\KBV-PROJETOS\api-wpp\env-kbv\Scripts\activate.bat"
-
-REM Roda o script Python
-python "C:\Users\Lucius\Documents\GitHub\KBV-PROJETOS\api-wpp\teste2.py"
-
-REM Desativa o ambiente virtual (opcional)
-deactivate
